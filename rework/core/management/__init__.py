52d300652d0dd13435496468778cb8b20875fd49
Josh.Yu <josh.yu_8@live.com>
2020-03-25 16:46:52 +0800

    Added scripts to generate project structure layout

--- rework/core/management/__init__.py
+++ rework/core/management/__init__.py
@@ -1,5 +1,20 @@
 import sys
 
+from . import project
+
+
+COMMANDS = {
+    'init': project.init,
+}
+
 
 def execute_from_command_line(argv=None):
-    print('Hello, Rework CLI, you argv is %s' % (argv or sys.argv[:]))
+    argv = argv or sys.argv[:]
+    print('Hello, Rework CLI, you argv is %s' % argv)
+
+    command = argv[1]
+
+    if command in COMMANDS:
+        COMMANDS.get(command)(argv[2:])
+    else:
+        print('[Error] Command not found!')
