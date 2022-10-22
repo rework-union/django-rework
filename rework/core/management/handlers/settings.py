fc64105448259c29f0c120c4cc19541e560a862f
JoshYuJump <josh.yu_8@live.com>
2022-10-23 02:46:03 +0800

    Changed logging settings

--- rework/core/management/handlers/settings.py
+++ rework/core/management/handlers/settings.py
@@ -115,7 +115,7 @@ LOGGING = {
     'disable_existing_loggers': False,
     'formatters': {
         "default": {
-            "format": '[%(asctime)s: %(levelname)s/%(name)s %(pathname)s:%(lineno)d] %(message)s',
+            "format": '[%(asctime)s: %(levelname)s/%(name)s %(filename)s:%(lineno)d] %(message)s',
         },
     },
     'handlers': {
