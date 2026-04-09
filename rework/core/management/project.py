84bc21793d5ef05176f9c8fdc7c7fa3932d76afa
JoshYuJump <josh.yu_8@live.com>
2026-04-09 16:01:25 +0800

    Removed `yapf` in init templates

--- rework/core/management/project.py
+++ rework/core/management/project.py
@@ -18,36 +18,36 @@ def init(params):
     base_dir = os.getcwd()
     project_dir = base_dir
 
-    say(f'Initialing project: ``{project}`` using `django-admin` command')
+    say(f"Initialing project: ``{project}`` using `django-admin` command")
     result = subprocess.run(["django-admin", "startproject", *params])
 
     if result.returncode != 0:
-        say(f'Initialized failed!', icon='🌶 ', wrap='C')
+        say(f"Initialized failed!", icon="🌶 ", wrap="C")
         return False
 
     # Changed the settings files
-    say(f'Changed the settings files to satisfy multi environments')
+    say(f"Changed the settings files to satisfy multi environments")
     settings_folder = os.path.join(project_dir, project)
     settings_handler = SettingsHandle(project=project, path=settings_folder)
 
     from rework import __version__
+
     # template variables
     kwargs = {
-        'django_rework_version': __version__,
-        'project': project,
+        "django_rework_version": __version__,
+        "project": project,
     }
 
     settings_handler.initialize()
 
     # fabric DevOps
-    copy_template_to_file('fabfile.py', base_dir, **kwargs)
+    copy_template_to_file("fabfile.py", base_dir, **kwargs)
 
     # Others
-    copy_template_to_file('.editorconfig', base_dir, **kwargs)
-    copy_template_to_file('.gitignore', base_dir, **kwargs)
-    copy_template_to_file('.style.yapf', base_dir, **kwargs)
-    copy_template_to_file('requirements.txt', base_dir, **kwargs)
-    copy_template_to_file('.env.dist', base_dir, **kwargs)
-    copy_template_to_file('.env', base_dir, **kwargs)
+    copy_template_to_file(".editorconfig", base_dir, **kwargs)
+    copy_template_to_file(".gitignore", base_dir, **kwargs)
+    copy_template_to_file("requirements.txt", base_dir, **kwargs)
+    copy_template_to_file(".env.dist", base_dir, **kwargs)
+    copy_template_to_file(".env", base_dir, **kwargs)
 
-    say(f'Initialized completely!', icon='🎨', wrap='C')
+    say(f"Initialized completely!", icon="🎨", wrap="C")
